93b2731fe1d1c4b1b2499b81a0a482eaf6981df4
canberko560 <canberkvarli@gmail.com>
2022-02-05 20:34:49 +0300

    mod

--- 02.26/algo.rb
+++ 02.26/algo.rb
@@ -113,3 +113,19 @@ end
 #Input: nums = [-1,0,3,5,9,12], target = 2
 #Output: -1
 #Explanation: 2 does not exist in nums so return -1
+
+
+def search(nums, target)
+    #iterate nums
+    #if i == target
+    # return index of i 
+    #else -1
+    
+    nums.each do |i|
+        if i == target 
+            return nums.index(i)
+        else
+           -1 
+        end
+    end
+end
