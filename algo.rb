00b6a69b7718939c9ceef3eef592d23f4732dd7c
canberko560 <canberkvarli@gmail.com>
2022-02-10 20:07:28 +0300

    add q

--- algo.rb
+++ algo.rb
@@ -360,3 +360,25 @@ end
 var reverseString = function(s) {
     s.reverse()
 };
+
+#Reverse Words in a String III
+
+#Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+ 
+
+#Example 1:
+
+#Input: s = "Let's take LeetCode contest"
+#Output: "s'teL ekat edoCteeL tsetnoc"
+
+def reverse_words(s)
+    #split the string by space
+    #iterate words and reverse every word
+    # push reversed word into result
+    # join it by space 
+    result = []
+    words = s.split(' ')
+    words.each {|word| result << word.reverse()}
+    return result.join(" ")
+end
