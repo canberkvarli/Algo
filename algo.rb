715c4447fe8d427ba215a1d31ffdb70e2325adac
canberko560 <canberkvarli@gmail.com>
2022-02-18 12:17:12 +0300

    add recursive

--- algo.rb
+++ algo.rb
@@ -386,4 +386,36 @@ def factorial(n)
     return n * factorial(n -1)
 end
 
-puts factorial(5)
+
+=begin
+// Write a method fib(n) that takes in a number and returns the nth number of
+// the fibonacci sequence.
+//
+// In the fibonacci sequence, the 1st number is 1 and the 2nd number is 1. To generate the
+// next number in the sequence, we take the sum of the previous two fibonacci numbers.
+// For example the first 5 numbers of the fibonacci sequence are `1, 1, 2, 3, 5`
+//
+// Examples:
+//
+// fib(1) // => 1
+// fib(2) // => 1
+// fib(3) // => 2
+// fib(4) // => 3
+// fib(5) // => 5
+// fib(6) // => 8
+// fib(7) // => 13
+=end
+
+def fib(n) 
+    # 3
+    # base case
+    return 1 if n == 1
+    return 1 if n == 2
+
+    # recursive case
+
+    return fib(n-2) + fib(n-1)
+    
+end
+
+puts fib(7)
